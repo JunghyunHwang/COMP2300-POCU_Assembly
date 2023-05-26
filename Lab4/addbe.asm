8001282208341ee5e3977f986caf5810f840d39a
Ja_Hwang <dmagk560@gmail.com>
2023-05-26 23:54:39 +0900

    Add addbe

---
+++ Lab4/addbe.asm
@@ -0,0 +1,22 @@
+ina=$4003
+inb=$4007
+out=$2300
+    ldx #$FF
+    txs
+
+    clc
+    lda ina
+    adc inb
+    sta out
+
+    lda ina-1
+    adc inb-1
+    sta out+1
+
+    lda ina-2
+    adc inb-2
+    sta out+2
+
+    lda ina-3
+    adc inb-3
+    sta out+3
