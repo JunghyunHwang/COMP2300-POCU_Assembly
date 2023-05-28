463909de46c94e27792442110ccbb7d974141c33
Ja_Hwang <dmagk560@gmail.com>
2023-05-28 15:43:50 +0900

    Remove terst

--- Lab4/divby2.asm
+++ Lab4/divby2.asm
@@ -2,9 +2,6 @@ masknegative=%10000000
 in=$4003
 out=$2303
 
-testin=$4013
-testout=$2313
-
     ldx #$FF
     txs
 
@@ -31,28 +28,3 @@ testout=$2313
     lda in-3
     ror
     sta out-3
-
-    ;Test positive number
-    lda testin
-    php
-    ror
-    sta testout
-    pla
-    and #masknegative
-    ora testout
-    sta testout
-
-    lda testin
-    ror
-
-    lda testin-1
-    ror
-    sta testout-1
-
-    lda testin-2
-    ror
-    sta testout-2
-
-    lda testin-3
-    ror
-    sta testout-3
