0985a5c7a8fdc08bb7a5e2bc48eaa27606ce980e
junghyunhwang <dmagk560@gmail.com>
2023-06-03 16:51:07 +0900

    Add variable temp_out

--- Lab5/shashac.asm
+++ Lab5/shashac.asm
@@ -1,92 +1,103 @@
 buffer=$C000
 out=$2300
-size=$0010
-check_bit=size+1
-count=$0020
+count=$0010
+check_bit=count+1
+temp_out=$0020
+
     ldx #$FF
     txs
 
     lda #0
-    sta out
-    sta out+1
-    sta out+2
-    sta out+3
+    sta temp_out
+    sta temp_out+1
+    sta temp_out+2
+    sta temp_out+3
     
     lda buffer+1
     sta check_bit
 
     lda buffer
-    sta size
     clc
-    adc size
-    adc size
-    adc size
+    adc buffer
+    adc buffer
+    adc buffer
     sta count
-
 round:
     ldx count
 
 accum:
-    lda out+3
+    lda temp_out+3
     eor buffer+1,x
-    sta out+3
+    sta temp_out+3
     dex
 
-    lda out+2
+    lda temp_out+2
     eor buffer+1,x
-    sta out+2
+    sta temp_out+2
     dex
 
-    lda out+1
+    lda temp_out+1
     eor buffer+1,x
-    sta out+1
+    sta temp_out+1
     dex
 
-    lda out
+    lda temp_out
     eor buffer+1,x
-    sta out
+    sta temp_out
     dex
     
     bne accum
 
 check:
-    lda out
+    lda temp_out
     and check_bit
     beq continue
     
-    lda out+1
+    lda temp_out+1
     and check_bit
     beq continue
 
-    lda out+2
+    lda temp_out+2
     and check_bit
     beq continue
 
-    lda out+3
+    lda temp_out+3
     and check_bit
     bne end
 
 continue:
-    lda #1
-    asl out
-    ora out
-    sta out
+    asl temp_out
+    rol temp_out+1
+    rol temp_out+2
+    rol temp_out+3
 
-    lda #1
-    rol out+1
-    ora out+1
-    sta out+1
+    lda temp_out
+    ora #1
+    sta temp_out
 
-    lda #1
-    rol out+2
-    ora out+2
-    sta out+2
-    
-    lda #1
-    rol out+3
-    ora out+3
-    sta out+3
+    lda temp_out+1
+    ora #1
+    sta temp_out+1
+
+    lda temp_out+2
+    ora #1
+    sta temp_out+2
+
+    lda temp_out+3
+    ora #1
+    sta temp_out+3
 
     jmp round
 
 end:
+    lda temp_out
+    sta out
+
+    lda temp_out+1
+    sta out+1
+
+    lda temp_out+2
+    sta out+2
+
+    lda temp_out+3
+    sta out+3
