ed3659897f454236f4516d5c3c20a9e055fd5260
Ja_Hwang <dmagk560@gmail.com>
2023-06-08 23:06:19 +0900

    Add diagonal

--- Assignment2/5bingo.asm
+++ Assignment2/5bingo.asm
@@ -31,52 +31,71 @@ callnum:
 ;=================
 won:
     .SUBROUTINE
+;=====diagonal=====
+    ldx #4
+    lda table,x
+    bpl .diagonal
 
-    ldx #LEN-1
-
-.set_y:
     ldy #5
-.h_loop:
-    txa
-    bmi .vertical
+.rl_loop
+    dey
+    beq .bingo
+
+    inx
+    inx
+    inx
+    inx
 
     lda table,x
-    bpl .dec_x
-    
-    dex
-    dey
-    bne .h_loop
+    bmi .rl_loop
 
+.diagonal
+    ldx #0
+    lda table,x
+    bpl .vertical
+
+    ldy #5
+.lr_loop
+    dey
+    beq .bingo
+
+    inx
+    inx
+    inx
+    inx
+    inx
+    inx
+
+    lda table,x
+    bmi .lr_loop
+
+    jmp .vertical
+
+.bingo
     lda #$01
     sta vret
     rts
 
-.dec_x:
-    dex
-    dey
-    bne .dec_x
-    beq .set_y
-
 ;=====vertical=====
 .vertical
-    ldx #$05
+    ldx #5
     stx vtmp
 
 .set
     ldx vtmp
     dex
-    bmi .ret
+    bmi .horizontal
     stx vtmp
 
-    ldy #$05
+    ldy #5
     
 .v_loop
     lda table,x
-    bmi .chk
+    bmi .chk_bingo
 
     jmp .set
 
-.chk:
+.chk_bingo:
     dey
     beq .bingo
     inx
@@ -86,16 +105,38 @@ won:
     inx
     
     lda table,x
-    bmi .chk
+    bmi .chk_bingo
 
     jmp .set
 
-.bingo
-    lda #$01
+;=====horizontal=====
+.horizontal
+    ldx #LEN-1
+
+.set_y:
+    ldy #5
+.h_loop:
+    txa
+    bmi .ret
+
+    lda table,x
+    bpl .dec_x
+    
+    dex
+    dey
+    bne .h_loop
+
+    lda #1
     sta vret
     rts
 
+.dec_x:
+    dex
+    dey
+    bne .dec_x
+    beq .set_y
+
 .ret
-    lda $00
+    lda #0
     sta vret
     rts
