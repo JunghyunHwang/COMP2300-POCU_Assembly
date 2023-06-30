4f692bae2004637666906c181df231a4e009d98b
JunghyunHwang <dmagk560@gmail.com>
2023-07-01 02:27:27 +0900

    Add sort

--- Lab7/sort.asm
+++ Lab7/sort.asm
@@ -7,20 +7,61 @@ TITLE Sort
 
 .DATA
 buffer DB 256 DUP (?)
-strbuf DW buffer
+str_buf DW buffer
+last_idx DW ?
 
 .CODE
 .STARTUP
-    push strbuf
+    push str_buf
     call read_string
     add sp, 2
 
-    mov cx, buffer[2]
-    
+    mov ax, [str_buf + 1]
+    mov dx, 2
+    mov cx, 0
+
+    mov last_idx, ax
+    add last_idx, 2
+
+loop_bubble:
+    mov si, 2
+    mov bx, last_idx
+    dec bx
+    sub bx, cx
+
+loop_swap:
+    mov ax, [str_buf][si]
+    cmp ax, [str_buf][si + 1]
+
+    jg swap
+
+    inc si
+    cmp bx, si
+    jne loop_swap
+
+    inc dx
+    inc cx
+    cmp last_idx, dx
+    jne loop_bubble
+
+    mov bx, last_idx
+    mov [str_buf][bx], '$'
+
+    mov al, 09h
+    lea dx, buffer
+    int 21h
+
     ; 프로그램 종료
     mov ah, 4Ch
     xor al, al
     int 21h
+
+swap:
+    xchg ax, [str_buf][si + 1]
+
+    inc si
+    cmp bx, si
+    jne loop_swap
 
 INCLUDE readstr.inc
 
