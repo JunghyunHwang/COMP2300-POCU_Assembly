dd98e781eb976ec7bbb4ab947ccf6e2ff7921750
JunghyunHwang <dmagk560@gmail.com>
2023-07-21 21:26:24 +0900

    Fix standard

--- Lab10/eprpc.c
+++ Lab10/eprpc.c
@@ -2,55 +2,55 @@
 
 void __declspec(naked) eprpc_init(void)
 {
-	__asm {
-		finit
-		ret
-	}
+    __asm {
+        finit
+        ret
+    }
 }
 
 void eprpc_load(const double num)
 {
-	__asm {
-		fld num
-	}
+    __asm {
+        fld num
+    }
 }
 
 void eprpc_get_result(double* out_result)
 {
-	__asm {
-		mov edx, [ebp+8]
-		fst QWORD PTR [edx]
-	}
+    __asm {
+        mov edx, [ebp+8]
+        fst QWORD PTR [edx]
+    }
 }
 
 void __declspec(naked) eprpc_add(void)
 {
-	__asm {
-		fadd
-		ret
-	}
+    __asm {
+        fadd
+        ret
+    }
 }
 
 void __declspec(naked) eprpc_sub(void)
 {
-	__asm {
-		fsub
-		ret
-	}
+    __asm {
+        fsub
+        ret
+    }
 }
 
 void __declspec(naked) eprpc_mul(void)
 {
-	__asm {
-		fmul
-		ret
-	}
+    __asm {
+        fmul
+        ret
+    }
 }
 
 void __declspec(naked) eprpc_div(void)
 {
-	__asm {
-		fdiv
-		ret
-	}
+    __asm {
+        fdiv
+        ret
+    }
 }
