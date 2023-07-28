9650e94c7ce5272972f8a76e7cee61cfa0fb3e63
JunghyunHwang <dmagk560@gmail.com>
2023-07-28 19:54:47 +0900

    Fix read points

--- Assignment4/main.c
+++ Assignment4/main.c
@@ -1,17 +1,24 @@
 #include "transformer.h"
 #include <assert.h>
-#include <string.h>
-
-#define ASSERT_MAT_EQUALS(expected, actual) assert(memcmp(expected, actual, sizeof(mat4_t)) == 0)
-#define ASSERT_VEC_EQUALS(expected, actual) assert(memcmp(expected, actual, sizeof(vec4_t)) == 0)
 
 int main(void)
 {
     enum { MAX_POINTS = 512 };
 
-    vec4_t points[MAX_POINTS];
+    vec4_t local_points[MAX_POINTS];
+    vec4_t world_points[MAX_POINTS];
+    size_t count;
 
-    size_t count = read_points(points, MAX_POINTS);
+    const vec3_t scale = { 1.f, 2.f, 1.f };
+    const vec3_t rotation = { 60.f, -30.f, 15.f };
+    const vec3_t translation = { 2.f, 0.f, 3.f };
+
+    count = read_points(local_points, MAX_POINTS);
+    assert(count <= MAX_POINTS);
+
+    run(world_points, local_points, count, &scale, &rotation, &translation);
+
+    print_csv(world_points, count);
 
     return 0;
 }
--- Assignment4/transformer.c
+++ Assignment4/transformer.c
@@ -19,13 +19,13 @@ size_t read_points(vec4_t* points, const size_t count)
 {
     vec4_t* p_points = points;
     size_t number_of_points = 0;
-    
-    for (size_t i = 0; i < count; ++i) {
-        if (scanf("%f", &p_points->x) == EOF) {
+
+    while (number_of_points < count) {
+        int ret = scanf("%f%f%f", &p_points->x, &p_points->y, &p_points->z);
+
+        if (ret == 0) {
             break;
         }
-
-        scanf("%f%f", &p_points->y, &p_points->z);
 
         ++p_points;
         ++number_of_points;
